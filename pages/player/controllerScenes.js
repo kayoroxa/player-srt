39982d4ce6a24382374cffdcbf7166da2175a684
Caio <kayoroxa@gmail.com>
2021-12-31 15:35:15 -0300

    fade, & opacity 0.5 when pause

--- pages/player/controllerScenes.js
+++ pages/player/controllerScenes.js
@@ -10,34 +10,65 @@ let indexTime = 0
 video.currentTime = convertTimeStr(times[indexTime][0])
 
 let canStop = true
+// let forceStopTeach = true
 
 document.addEventListener('keydown', e => {
-  console.log(e.key)
+  // console.log(e.key)
   if (e.key === 'PageUp' && indexTime < times.length - 1) {
+    fadeIn(0.01)
     indexTime++
     video.currentTime = convertTimeStr(times[indexTime][0])
     video.play()
     canStop = true
   } else if (e.key === 'PageDown' && indexTime > 0) {
+    fadeIn(0.01)
     indexTime--
     video.currentTime = convertTimeStr(times[indexTime][0])
     video.play()
     canStop = true
   }
   if (e.key === 'd') {
+    fadeIn()
     video.currentTime = subtitlesDataEn[indexSub + 1].startTime
   } else if (e.key === 'a') {
+    fadeIn()
     video.currentTime = subtitlesDataEn[indexSub - 1].startTime
   } else if (e.key === 's') {
+    fadeIn()
     video.currentTime = lastSubtitleEn.startTime
   }
   if (e.key === ' ') {
     e.preventDefault()
     if (video.paused) {
+      video.classList.remove('paused')
+      document.querySelector('.subtitles').classList.remove('hide')
       video.play()
     } else {
-      video.pause()
+      if (!video.controls) {
+        document.querySelector('.subtitles').classList.add('hide')
+        video.classList.add('paused')
+      }
+      const timer = setInterval(() => {
+        if (video.volume > 0) {
+          video.volume -= 0.1
+          if (video.volume - 0.1 < 0) {
+            video.pause()
+            video.volume = 0
+          }
+        } else {
+          clearInterval(timer)
+        }
+      }, 20)
     }
+  }
+  if (e.key === 'ArrowRight') {
+    video.currentTime += 1
+  }
+  if (e.key === 'ArrowLeft') {
+    video.currentTime -= 1
+  }
+  if (e.key === 'c') {
+    video.controls = !video.controls
   }
 })
 
@@ -48,4 +79,24 @@ video.addEventListener('timeupdate', () => {
     if (canStop) video.pause()
     canStop = false
   }
+})
+
+function fadeIn(increment = 0.1) {
+  console.log('fadeIn')
+  video.volume = 0
+  const timer = setInterval(() => {
+    if (video.volume < 1) {
+      video.volume += increment
+      if (video.volume + increment > 1) {
+        video.volume = 1
+      }
+    } else {
+      clearInterval(timer)
+    }
+  }, 20)
+}
+//on change current time
+
+video.addEventListener('play', () => {
+  fadeIn()
 })
