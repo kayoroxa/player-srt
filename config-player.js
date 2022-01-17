8dc838046c6da3575d0bb9c18520ab7fa431ad80
Caio <kayoroxa@gmail.com>
2022-01-17 12:21:07 -0300

    config player

--- config-player.js
+++ config-player.js
@@ -1,57 +1,38 @@
 module.exports = {
   folderMovies: 'F:/movies',
-  findTeach: 'have a nice',
+  // findTeach: 'have a nice',
   start: '0:0:0',
   times: [
-    // ['1:27:50', '1:28:16'],
-    // ['0:33:40', '0:34:28'],
-    // ['0:19:1', '0:19:34'],
-    // ['0:32:14', '0:32:55'],
-    // ['0:36:11', '0:36:35'],
-    // ['0:50:27', '0:51:20'],
-    // ['1:10:42', '1:11:25'],
-    // ['0:59:46', '1:0:47'],
-
-    /*------encanto----->>>*/
-
-    // ['0:27:35', '0:28:3'],
-    // ['1:21:47', '1:22:34'],
-    // ['1:23:20', '1:23:50'],
-    // ['0:29:42', '0:30:29'],
-    ['0:56:28', '0:56:58'],
-    ['1:12:55', '1:13:25'],
-    ['0:16:31', '0:16:51'],
-    // ['0:45:1', '0:45:30'],
+    //----- Stuart Little (1999) ---- //
+    // ['0:4:7', '0:4:34'],
+    // ['0:1:33', '0:2:1'],
+    // ['0:18:17', '0:18:53'],
+    // ['1:16:37', '1:17:16'],
+    // ['1:2:48', '1:3:12'],
+    // ['0:45:4', '0:45:34'],
+    //----- casa fantasma ---- //
+    // ['0:51:26', '0:51:58'],
+    //------ Toy Story ------ //
+    // ['1:1:29', '1:1:55'],
+    ['0:10:25', '0:10:59'],
+    // ['0:9:7', '0:9:36'],
   ],
-  // path: 'F:/movies/Ratatouille (2007) [1080p]',
-  path: 'F:/movies/Encanto (2021) [720p] [WEBRip] [YTS.MX]',
+  // path: 'F:/movies/Toy Story (1995)',
+  // path: 'F:/movies/Monster House (2006)',
+  path: 'F:/movies/Stuart Little (1999)',
   teaches: [
-    /*------encanto----->>>*/
-    'No matter',
-    'will never be strong enough',
-    "won't be perfect enough",
-    'you only saw the worst in',
-    'care about',
-    "You're the one ",
-
-    /*------encanto----->>>*/
-    // 'What did you put in this',
-    // "Where'd you get that",
-    // 'steal',
-    // "I'll buy some spices",
-    // "We're going to be late",
-    // 'And on the',
-    // 'stupid',
-    // 'weird',
-    // 'neither of us',
-    // 'we got to do it together',
-    // //cena 2
-    // 'So this is it',
-    // 'I mean',
-    // "it's not much",
-    // 'It could be worse',
-    // "There's heat",
-    // 'couch',
-    // `what's mine is yours`,
+    //------ Toy Story ------ //
+    'gangway',
+    'find out',
+    'those', //what is in those presents
+    "Who's hungry",
+    'Here come',
+    "I've got",
+    'What in the world',
+    'thought',
+    'to pick these up',
+    "Shouldn't", //Shouldn't they be there by now
+    "What's taking them so long",
+    "They're not lying down on the job",
   ],
 }
