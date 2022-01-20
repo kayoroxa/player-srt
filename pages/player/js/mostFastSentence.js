608e8fdec43ef852b5feeb18cf98c1ca660fab5c
Caio <kayoroxa@gmail.com>
2022-01-20 04:13:51 -0300

    change filter most fast sentence

--- pages/player/js/mostFastSentence.js
+++ pages/player/js/mostFastSentence.js
@@ -1,21 +1,23 @@
 const obs = require('../../../utils/observer')
 
 function getMostFastSentences(subtitlesData) {
-  const sentences = subtitlesData.map(sub => {
-    const text = sub.text.split(/[\s|\n]/g)
-    const lettersPerSecond =
-      sub.text.split().filter(v => v.match(/[a-zA-Z]/g)).length /
-      (sub.endTime - sub.startTime)
-    const wordPerSecond = text.length / (sub.endTime - sub.startTime)
-    return {
-      text,
-      startTime: sub.startTime,
-      endTime: sub.endTime,
-      wordPerSecond: Math.round(wordPerSecond),
-      lettersPerSecond: Math.round(lettersPerSecond),
-      durationClip: Math.round(sub.endTime - sub.startTime),
-    }
-  })
+  const sentences = subtitlesData
+    .map(sub => {
+      const text = sub.text.split(/[\s|\n]/g)
+      const lettersPerSecond =
+        sub.text.split().filter(v => v.match(/[a-zA-Z]/g)).length /
+        (sub.endTime - sub.startTime)
+      const wordPerSecond = text.length / (sub.endTime - sub.startTime)
+      return {
+        text,
+        startTime: sub.startTime,
+        endTime: sub.endTime,
+        wordPerSecond: Math.round(wordPerSecond),
+        lettersPerSecond: Math.round(lettersPerSecond),
+        durationClip: Math.round(sub.endTime - sub.startTime),
+      }
+    })
+    .filter(sub => sub.text.length > 10)
   const mostFastSentences = sentences.sort((a, b) => {
     return b.wordPerSecond - a.wordPerSecond
   })
