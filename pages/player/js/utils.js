01d4332d13443b26ee15e6dc4b7a29773591b5ce
Caio <kayoroxa@gmail.com>
2022-01-23 00:51:17 -0300

    findVideoPath utils

---
+++ pages/player/js/utils.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const pathJoin = require('path').join
+
+function findVideoPath(options) {
+  const files = fs.readdirSync(options.pathMovie)
+  let file
+  let types
+
+  if (Array.isArray(options.type)) {
+    types = options.type
+  } else {
+    types = [options.type]
+  }
+
+  if (!options.type) types = ['.mp4', '.mkv', '.avi', '.mov']
+
+  if (!options?.pt) {
+    file = files.find(
+      file =>
+        types.some(type => file.toLowerCase().endsWith(type)) &&
+        !file.includes('port') &&
+        !file.includes('pt')
+    )
+  } else {
+    file = files.find(
+      file =>
+        types.some(type => file.toLowerCase().endsWith(type)) &&
+        (file.includes('port') || file.includes('pt'))
+    )
+  }
+  if (!file) {
+    return
+  } else {
+    return pathJoin(options.pathMovie, file)
+  }
+}
+
+module.exports = {
+  findVideoPath,
+}
