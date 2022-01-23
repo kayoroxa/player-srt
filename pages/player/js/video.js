c4d38d4c0635dd7a7eb8ab3b3f18f3324e80cec4
Caio <kayoroxa@gmail.com>
2022-01-23 02:41:18 -0300

    video observer

---
+++ pages/player/js/video.js
@@ -0,0 +1,34 @@
+const obs = require('../../../utils/observer')
+
+obs('video').on('srcChange', ({ src }) => {
+  const lastVideo = document.querySelector('video')
+  lastVideo.muted = true
+
+  const newVideo = lastVideo.cloneNode(true)
+  newVideo.src = src
+  newVideo.classList.add('hide')
+
+  // //add new video
+  document.querySelector('.player').appendChild(newVideo)
+
+  // //when video loaded
+  //when video play
+  // newVideo.onplay = () => {
+  newVideo.onplay = me => {
+    console.log('loadedmetadata')
+    // obs('video').on('newVideoCanShow', () => {
+    me.target.classList.remove('hide')
+
+    setTimeout(() => {
+      lastVideo.remove()
+    }, 200)
+    // })
+  }
+})
+
+obs('video').on('timeChange', time => {
+  //get last video element children
+  document.querySelector('video:last-child').currentTime = time
+
+  // obs('video').notify('newVideoCanShow')
+})
