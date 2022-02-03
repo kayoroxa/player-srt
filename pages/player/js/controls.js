f63e90a1ab98f2ce86d1a408bab29340a810b5ef
Caio <kayoroxa@gmail.com>
2022-02-03 01:58:58 -0300

    controls

--- pages/player/js/controls.js
+++ pages/player/js/controls.js
@@ -1,6 +1,9 @@
 const obs = require('../../../utils/observer')
+let shortCutActive = true
 
 document.addEventListener('keydown', function (e) {
+  if (!shortCutActive) return
+
   if (e.key.toLowerCase() === 'p') {
     obs('CONTROL').notify('find-next')
   } else if (e.key.toLowerCase() === 'o') {
@@ -17,9 +20,16 @@ document.addEventListener('keydown', function (e) {
     obs('CONTROL').notify('subtitle-current')
   } else if (e.key.toLowerCase() === 'r') {
     obs('CONTROL').notify('subtitle-repeat-toggle')
-  } else if (e.key.toLowerCase() === 'c') {
-    obs('CONTROL').notify('video-control-toggle')
   } else if (e.key.toLowerCase() === 'l') {
-    obs('CONTROL').notify('video-subtitle-toggle')
+    obs('CONTROL').notify('subtitle-show-toggle')
+  } else if (e.key.toLowerCase() === 'c') {
+    obs('CONTROL').notify('video-config', 'control-toggle')
+  } else if (e.key === ' ') {
+    obs('CONTROL').notify('video-play-toggle', e)
   }
+})
+
+obs('CONTROL').on('shortcut-toggle', value => {
+  if (value !== undefined) shortCutActive = value
+  else shortCutActive = !shortCutActive
 })
