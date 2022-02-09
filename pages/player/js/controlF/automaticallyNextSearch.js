c12b37e7df408552caaf7390649212c85e8bfd75
Caio <kayoroxa@gmail.com>
2022-02-08 23:46:02 -0300

    fix prev subtitle when automatically next find

--- pages/player/js/controlF/automaticallyNextSearch.js
+++ pages/player/js/controlF/automaticallyNextSearch.js
@@ -2,9 +2,11 @@ const obs = require('../../../../utils/observer')
 
 let toggle = false
 let ignoreLast = true
+let tempDisabled = false
 
 obs('CONTROL').on('toggle-automatically-next-search', () => {
-  toggle = !toggle
+  toggle = !tempDisabled ? !toggle : true
+  tempDisabled = false
   obs('warning').notify('show', {
     title: 'Automatically next search',
     message: toggle ? 'ON' : 'OFF',
@@ -14,12 +16,23 @@ obs('CONTROL').on('toggle-automatically-next-search', () => {
 obs('search').on('prevSearchClick', () => {
   ignoreLast = false
 })
+
 obs('search').on('nextSearchClick', () => {
+  if (toggle) tempDisabled = false
+  ignoreLast = false
+})
+
+obs('CONTROL').on('subtitle-prev', () => {
+  tempDisabled = true
+  ignoreLast = false
+})
+
+obs('CONTROL').on('subtitle-next', () => {
   ignoreLast = false
 })
 
 obs('subtitle').on('sentence-sub-end', () => {
   ignoreLast = !ignoreLast
-  if (!toggle || ignoreLast) return
+  if (!toggle || ignoreLast || tempDisabled) return
   obs('search').notify('nextSearchClick')
 })
