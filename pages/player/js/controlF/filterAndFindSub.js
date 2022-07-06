d5485d0ae315fbd0c2678ae3d0ff3cc70695fa7d
Caio <kayoroxa@gmail.com>
2022-07-06 20:28:36 -0300

    feacture 2 folders movie

--- pages/player/js/controlF/filterAndFindSub.js
+++ pages/player/js/controlF/filterAndFindSub.js
@@ -5,44 +5,45 @@ const fs = require('fs')
 const subtitle = require('../../Subtitle')
 
 function finderSrtAllMovies({ query, exactly, sameMovie }) {
-  const pathMovies = configPlayer.folderMovies
+  const pathsMovies = [configPlayer.folderMovies, configPlayer.folderMovies2]
 
-  if (!fs.existsSync(pathMovies)) {
-    // obs('warning').notify('show', {
-    //   title: 'Não foi possível encontrar o diretório de filmes',
-    //   message:
-    //     'Verifique se o "folderMovies" está correto no "config-player.js"',
-    // })
-    return false
-  }
-  let files = []
-  if (sameMovie) files = [subtitle.infoPath.folder.replace(/.*\//g, '')]
-  else files = fs.readdirSync(pathMovies) || false
   let find = []
 
-  files.forEach(file => {
-    const path = pathJoin(pathMovies, file)
-    const subtitleEn = readSrt(path)
-    if (!subtitleEn) return
-    // const isQueryRegex = query[0] === '*'
+  pathsMovies.forEach(pathMovies => {
+    let files = []
 
-    query = query.replace(/__*/g, '_')
-    query = query.replace(/_/g, '.*')
-    const regex = new RegExp(`(?<=\\s|^)(${query})(?=\\s|$)`, 'ig')
-    const regexGeral = new RegExp(`(${query})`, 'ig')
+    if (!fs.existsSync(pathMovies)) {
+      return false
+    }
 
-    const subFind = subtitleEn.filter(sub => {
-      if (exactly && sub.text.toLowerCase().match(regex)) return true
-      else if (!exactly && sub.text.toLowerCase().match(regexGeral)) return true
+    if (sameMovie) files = [subtitle.infoPath.folder.replace(/.*\//g, '')]
+    else files = fs.readdirSync(pathMovies) || false
+
+    files.forEach(file => {
+      const path = pathJoin(pathMovies, file)
+      const subtitleEn = readSrt(path)
+      if (!subtitleEn) return
+      // const isQueryRegex = query[0] === '*'
+
+      query = query.replace(/__*/g, '_')
+      query = query.replace(/_/g, '.*')
+      const regex = new RegExp(`(?<=\\s|^)(${query})(?=\\s|$)`, 'ig')
+      const regexGeral = new RegExp(`(${query})`, 'ig')
+
+      const subFind = subtitleEn.filter(sub => {
+        if (exactly && sub.text.toLowerCase().match(regex)) return true
+        else if (!exactly && sub.text.toLowerCase().match(regexGeral))
+          return true
+      })
+      if (subFind.length <= 0 || !subFind) return
+
+      find.push(
+        ...subFind.map(sub => ({
+          moviePath: path,
+          subFind: sub,
+        }))
+      )
     })
-    if (subFind.length <= 0 || !subFind) return
-
-    find.push(
-      ...subFind.map(sub => ({
-        moviePath: path,
-        subFind: sub,
-      }))
-    )
   })
   return find
 }
